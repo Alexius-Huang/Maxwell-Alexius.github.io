441a9ba636dd97371afae21325d6af07031fff78
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-10-05 15:49:32 +0800

    Setting up React Router

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter, Switch, Route, Link } from 'react-router-dom'
+
+import Home from './components/home.js'
+import Resumé from './components/resumé.js'
 
 class Main extends React.Component {
   constructor(props) {
     super(props)
   }
-  
+
   render() {
-    return <div>Hello World</div>
+    return (
+      <main>
+        This is the Main Component!
+        <Switch>
+          <Route exact path="/"       component={ Home   } />
+          <Route exact path="/resumé" component={ Resumé } />
+        </Switch>
+        <Link to="resumé" onClick={this.handleClick.bind(this)}>Resumé</Link>
+      </main>
+    )
   }
 }
 
 ReactDOM.render(
-  <Main />,
+  <HashRouter>
+    <Main />
+  </HashRouter>,
   document.getElementById('app')
 )
---
+++ js/components/resumé.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Resumé extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div>This is Resumé Component</div>
+  }
+}
